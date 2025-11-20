49cd67577ec4f7a215f649be4d761b5fc812083d
Yunil-dev <yunil.official.dev@gmail.com>
2025-11-20 19:35:51 +0900

    chore: validate 기능 비활성화

--- src/apis/auth/index.ts
+++ src/apis/auth/index.ts
@@ -1,6 +1,10 @@
-import { api } from '@/apis';
-import { GetUserValidateResponse } from '@/models/auth';
+// validate 기능 주석 처리
+// import { api } from '@/apis';
+// import { GetUserValidateResponse } from '@/models/auth';
 
-export const getUserValidate = async () => {
-  return await api.get<GetUserValidateResponse>('/user/validate');
-};
+// export const getUserValidate = async () => {
+//   return await api.get<GetUserValidateResponse>('/user/validate');
+// };
+
+// 모듈 유지를 위한 더미 export
+export {};
