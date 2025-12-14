63b821f76d82e0e61d9b6549da22484fae03e256
Yunil-dev <yunil.official.dev@gmail.com>
2025-12-14 17:36:40 +0900

    feat: 유효성 검사를 위한 input 컴포넌트 개발

---
+++ src/components/input/index.styles.ts
@@ -0,0 +1,69 @@
+import { css } from '@emotion/react';
+import { theme } from '@/styles';
+import { Service } from '../../types';
+
+export const wrapper = css`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  width: 100%;
+`;
+
+export const label = css`
+  margin-bottom: 9px;
+`;
+
+export const inputWrapper = css`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+`;
+
+export const input = ({ errorMessage }: { errorMessage?: string }) => css`
+  display: flex;
+  flex: 1;
+
+  padding: 6px 0 10px 6px;
+  border-radius: 0;
+
+  text-align: left;
+
+  border-bottom: ${errorMessage
+    ? `1px solid ${theme.colors.red200}`
+    : `1px solid ${theme.colors.gray200}`};
+
+  &:focus {
+    border-bottom: 1px solid ${theme.colors.blue200};
+  }
+
+  ${theme.typo.body8};
+
+  &::placeholder {
+    color: ${theme.colors.gray300};
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+
+    margin: 0;
+  }
+
+  &[type='button'] {
+    cursor: pointer;
+  }
+
+  &:disabled {
+    color: ${theme.colors.gray300};
+  }
+  transition: border-bottom 0.2s ease;
+`;
+
+export const infoTextWrapper = css`
+  position: absolute;
+  bottom: -20px;
+  left: 2px;
+
+  padding: 0 2px;
+`;
---
+++ src/components/input/index.tsx
@@ -0,0 +1,86 @@
+import { forwardRef, InputHTMLAttributes, ReactNode, useImperativeHandle, useRef } from 'react';
+import { input, wrapper, label as labelCss, infoTextWrapper, inputWrapper } from './index.styles';
+import { Text } from '../text';
+
+export interface InputRef {
+  focus: () => void;
+}
+
+export interface Props extends Omit<InputHTMLAttributes<HTMLInputElement>, 'prefix' | 'suffix'> {
+  label?: string;
+  prefix?: ReactNode;
+  suffix?: ReactNode;
+  confirmMessage?: string;
+  errorMessage?: string;
+}
+
+export const Input = forwardRef<InputRef, Props>(
+  (
+    {
+      type = 'text',
+      label,
+      disabled,
+      spellCheck = false,
+      autoComplete = 'off',
+      prefix,
+      suffix,
+      confirmMessage,
+      errorMessage,
+      required,
+      ...props
+    },
+    ref
+  ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useImperativeHandle(ref, () => inputRef.current as InputRef);
+
+    return (
+      <div css={wrapper}>
+        {label && (
+          <>
+            <Text typo="subtitle3" css={labelCss}>
+              {label}{' '}
+              {required && (
+                <Text typo="body12" color="red200">
+                  *
+                </Text>
+              )}
+            </Text>
+          </>
+        )}
+
+        <div css={inputWrapper}>
+          {prefix && prefix}
+          <input
+            ref={inputRef}
+            type={type}
+            spellCheck={spellCheck}
+            autoComplete={autoComplete}
+            disabled={disabled}
+            css={input({ errorMessage })}
+            {...props}
+          />
+          {suffix && suffix}
+        </div>
+
+        {!errorMessage && confirmMessage && (
+          <div css={infoTextWrapper}>
+            <Text typo="body12" color="blue200">
+              {confirmMessage}
+            </Text>
+          </div>
+        )}
+
+        {errorMessage && (
+          <div css={infoTextWrapper}>
+            <Text typo="body12" color="red200">
+              {errorMessage}
+            </Text>
+          </div>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = 'Input';
