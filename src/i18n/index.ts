cbb98a7d8c5b735d0a3f23cc56e20d5e13053e36
Yunil-dev <yunil.official.dev@gmail.com>
2025-12-24 13:33:21 +0900

    feat: 다국어 i18n 라우팅/로딩 파이프라인 구축

---
+++ src/i18n/index.ts
@@ -0,0 +1,4 @@
+export { routing, defaultLocale, locales, type Locale, type ContentLocale } from './routing';
+export { getMergedMessages } from './getMergedMessages';
+export { getI18nConfig, detectLocale } from './request';
+export { I18nLink as Link, useChangeLocale, useCurrentLocale } from './navigation';
